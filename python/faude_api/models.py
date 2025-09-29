331489f310a6ca1d54d0e99655686b28ca69d6cf
juanjshb <168491243+juanjshb@users.noreply.github.com>
2025-09-29 16:43:21 -0400

    Delete python/faude_api directory

--- python/faude_api/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, condecimal
-from typing import Literal
-
-class Transaccion(BaseModel):
-    cliente_id: str = Field(..., description="Identificador interno del cliente (se anonimiza)")
-    monto: condecimal(gt=0)  # monto > 0
-    hora: int = Field(..., ge=0, le=23)
-    pais: Literal["DO", "US", "VE", "HT"]  # limitar a países válidos
